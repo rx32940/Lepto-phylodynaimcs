7661f0f507aa502198044f3d47df64793555d144
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-06-14 12:11:11 -0400

    try on sapleo2

--- bwa_sapelo2.sh
+++ bwa_sapelo2.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
+#PBS -q batch                                                            
+#PBS -N lepto_snps                                            
+#PBS -l nodes=1:ppn=2 -l mem=20gb                                        
+#PBS -l walltime=10:00:00                                                
+#PBS -M rx32940@uga.edu                                                  
+#PBS -m abe                                                              
+#PBS -o /scratch/rx32940/bwa_results                        
+#PBS -e /scratch/rx32940/bwa_results                        
+#PBS -j oe     
 
-cd "$(dirname "$0")"
 
-source activate bwa #activate bwa env.
+cd $PBS_O_WORKDIR
 
-path='bwa_results'# lead all output file to this folder
+module load BWA/0.7.15-foss-2016b
+module load SAMtools/1.9-foss-2016b
+module load BCFtools/1.9-foss-2016b 
+
+
+seq_path='/scratch/rx32940/lepto_wgs_seq'# lead all output file to this folder
+o_path='/scratch/rx32940/bwa_results'
 
 # index the reference
-bwa index Lai_56601.fasta
+bwa index $seq_path/Lai_56601.fasta
 
 # align the contigs to reference 
-echo $path
-bwa mem -t2 Lai_56601.fasta JQPC01.fasta > $path/JQPC01.sam
+bwa mem -t2 $seq_path/Lai_56601.fasta $seq_path/JQPC01.fasta > $o_path/JQPC01.sam
 # -o output, convert to binary, bam, format
-samtools sort -o $path/JQPC01.bam $path/JQPC01.sam
+samtools sort -o $o_path/JQPC01.bam $o_path/JQPC01.sam
 #index the alignment file
-samtools index $path/JQPC01.bam
+samtools index $o_path/JQPC01.bam
 # producing genotype likelihoods in VCF or BCF format
-bcftools mpileup -Ou -f Lai_56601.fasta $path/JQPC01.bam > $path/JQPC01.bcf
-bcftools call -mv -Ob $path/JQPC01.bcf > $path/JQPC01_final.bcf
-bcftools view -i '%QUAL>=20' $path/JQPC01_final.bcf > $path/JQPC01_final.vcf
+bcftools mpileup -Ou -f $seq_path/Lai_56601.fasta $o_path/JQPC01.bam > $o_path/JQPC01.bcf
+bcftools call -mv -Ob $o_path/JQPC01.bcf > $o_path/JQPC01_final.bcf
+bcftools view -i '%QUAL>=20' $o_path/JQPC01_final.bcf > $o_path/JQPC01_final.vcf
 
 
 
