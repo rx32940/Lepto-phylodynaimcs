911f2e66bf04f032c07c3498107ae33277528036
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-06-14 14:53:02 -0400

    db

--- bwa_sapelo2.sh
+++ bwa_sapelo2.sh
@@ -20,6 +20,8 @@ module load BCFtools/1.9-foss-2016b
 seq_path='/scratch/rx32940/lepto_wgs_seq'
 o_path='/scratch/rx32940/bwa_results'
 
+# index the reference
+bwa index $seq_path/Lai_56601.fasta
 
 for file in $seq_path/*.fasta; do
     echo "in loop"
@@ -28,11 +30,8 @@ for file in $seq_path/*.fasta; do
     echo $isolate
     if [ "$isolate" != "Lai_56601" ]; then
         echo "in if"
-        # index the reference
-        echo $seq
-        bwa index $seq_path/Lai_56601.fasta
         # align the contigs to reference 
-        bwa mem -t2 $seq_path/Lai_56601.fasta $file.fasta > $o_path/$isolate.sam
+        bwa mem -t2 $seq_path/Lai_56601.fasta $seq_path/$isolate.fasta > $o_path/$isolate.sam
         # -o output, convert to binary, bam, format
         samtools sort -o $o_path/$isolate.bam $o_path/$isolate.sam
         #index the alignment file
