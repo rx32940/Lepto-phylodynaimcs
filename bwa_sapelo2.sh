2fc9ad63dd52e356adcf8cec8f64e4eb314257f8
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-06-18 16:53:15 -0400

    debug

--- bwa_sapelo2.sh
+++ bwa_sapelo2.sh
@@ -18,8 +18,8 @@ module load BCFtools/1.9-foss-2016b
 module load picard/2.16.0-Java-1.8.0_144 
 
 
-seq_path='/scratch/rx32940/lepto_wgs_seq'
-o_path='/scratch/rx32940/bwa_results'
+seq_path='/scratch/rx32940/lepto_wgs_seq' # path to genomes
+o_path='/scratch/rx32940/bwa_results' # path to output files
 
 # index the reference
 bwa index $seq_path/Lai_56601.fasta
@@ -34,6 +34,7 @@ for file in $seq_path/*.fasta; do
         bwa mem -t2 $seq_path/Lai_56601.fasta $seq_path/$isolate.fasta > $o_path/$isolate.sam
         # -o output, convert to binary, bam, format
         samtools sort -o $o_path/$isolate.bam $o_path/$isolate.sam
+        # mark duplicates, this step isn't working. necessary for contigs?
         time java -Xmx20g -classpath "/usr/local/apps/eb/picard/2.16.0-Java-1.8.0_144" -jar  /usr/local/apps/eb/picard/2.16.0-Java-1.8.0_144/picard.jar MarkDuplicates I=$o_path/$isolate.bam O=$o_path/${isolate}_marked_dup.bam M=$o_path/${isolate}_md_metrics.txt
         #index the alignment file (bam)
         samtools index $o_path/${isolate}_marked_dup.bam
@@ -43,7 +44,7 @@ for file in $seq_path/*.fasta; do
 done
         
         # mpile up the bam lists
-        bcftools mpileup -Ou -f $seq_path/Lai_56601.fasta $o_path/bam_list.list > $o_path/multisample.bcf
+        bcftools mpileup -Ou -f -b $seq_path/Lai_56601.fasta bam_list.list > $o_path/multisample.bcf
         #call snps
         bcftools call -mv -Ob $o_path/multisample.bcf > $o_path/multisample_final.bcf
         # filter those with quality score less than 20
