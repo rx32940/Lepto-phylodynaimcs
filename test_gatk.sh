0ba37f8ca79860aa90d130d3e912b6930a960a9c
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-06-21 12:21:45 -0400

    test single

---
+++ test_gatk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -q batch                                                            
+#PBS -N lepto_snps                                            
+#PBS -l nodes=1:ppn=2 -l mem=20gb                                        
+#PBS -l walltime=10:00:00                                                
+#PBS -M rx32940@uga.edu                                                  
+#PBS -m abe                                                              
+#PBS -o /scratch/rx32940/bwa_results                        
+#PBS -e /scratch/rx32940/bwa_results                        
+#PBS -j oe     
+
+
+cd $PBS_O_WORKDIR
+
+module load GATK/4.0.11.0-foss-2016b-Python-2.7.14
+
+gatk HaplotypeCaller -R $seq_path/Lai_56601.fasta -I $o_path/JQOL00000000_sorted.bam -O $o_path/JQOL00000000_test.vcf -ERC GVCF
