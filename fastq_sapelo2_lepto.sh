cd01b4aad8f476fd5bf9f3fdf835a1b373323a47
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-06-26 17:06:14 -0400

    to cluster

--- fastq_sapelo2_lepto.sh
+++ fastq_sapelo2_lepto.sh
@@ -13,6 +13,6 @@ cd $PBS_O_WORKDIR
 
 module load SRA-Toolkit/2.9.1-centos_linux64
 
-cat /scratch/rx32940/SRR_Acc_List.txt | xargs -I{} fastq-dump -O /project/lslab/lab_shared/LeptoFastqSRA {}
+cat /scratch/rx32940/SRR_Acc_List.txt | xargs -I{} fastq-dump --gzip -O /project/lslab/lab_shared/LeptoFastqSRA {}
 
 
