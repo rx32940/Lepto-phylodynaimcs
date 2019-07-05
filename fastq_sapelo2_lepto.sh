9ea8fe9534cb5ad3db09cdb115c7de1fff16342a
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-07-05 10:49:49 -0400

    fix -o path

--- fastq_sapelo2_lepto.sh
+++ fastq_sapelo2_lepto.sh
@@ -13,6 +13,6 @@ cd $PBS_O_WORKDIR
 
 module load SRA-Toolkit/2.9.1-centos_linux64
 
-cat /scratch/rx32940/SRR_Acc_List.txt | xargs -I{} fastq-dump --gzip -O LeptoFastqSRA/LeptoFastqSRA {}
+cat /scratch/rx32940/SRR_Acc_List.txt | xargs -I{} fastq-dump --gzip -O /scratch/rx32940/LeptoFastqSRA {}
 
 
