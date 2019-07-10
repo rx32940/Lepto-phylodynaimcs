2f90b0c1ceb9f38f55216d5a27b8d5e04d710c8e
Ruijie Xu <rx32940@sapelo2-sub1.ecompute>
2019-07-10 12:39:32 -0400

    change file name

---
+++ download_fastq_lepto.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -q batch                                                            
+#PBS -N lepto_fastq                                            
+#PBS -l nodes=1:ppn=2 -l mem=20gb                                        
+#PBS -l walltime=30:00:00                                                
+#PBS -M rx32940@uga.edu                                                  
+#PBS -m abe                                                              
+#PBS -o /scratch/rx32940                        
+#PBS -e /scratch/rx32940                        
+#PBS -j oe     
+
+cd $PBS_O_WORKDIR
+
+module load SRA-Toolkit/2.9.1-centos_linux64
+
+cat /scratch/rx32940/SRR_Acc_List.txt | xargs -I{} fastq-dump --gzip -O /scratch/rx32940/LeptoFastqSRA {}
+
+
