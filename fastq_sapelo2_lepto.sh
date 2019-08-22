97a933be8aaacedde0da79b5d3db3fc62ffd64bc
Ruijie Xu <rx32940@sapelo2-sub2.ecompute>
2019-08-22 16:33:15 -0400

    commit

--- fastq_sapelo2_lepto.sh
+++ fastq_sapelo2_lepto.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-#PBS -q batch                                                            
-#PBS -N lepto_fastq                                            
-#PBS -l nodes=1:ppn=2 -l mem=20gb                                        
-#PBS -l walltime=20:00:00                                                
-#PBS -M rx32940@uga.edu                                                  
-#PBS -m abe                                                              
-#PBS -o /scratch/rx32940                        
-#PBS -e /scratch/rx32940                        
-#PBS -j oe     
+    
 
 cd $PBS_O_WORKDIR
 
